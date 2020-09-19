f0ca0b76c3bf62b003d867bb07592cf274c3a798
ALKALiKong <54100342+ALKALiKong233@users.noreply.github.com>
2020-09-19 09:48:09 +0800

    Update parser.sh

--- parser.sh
+++ parser.sh
@@ -3,8 +3,8 @@
 # CI Runner Script for Generation of blobs
 #
 
-setup_makefiles="/mnt/compile3/derpfest/device/xiaomi/ginkgo/setup-makefiles.sh"
-extract_files="/mnt/compile3/derpfest/device/xiaomi/ginkgo/extract-files.sh"
+setup_makefiles="/home/alk/yaap/device/xiaomi/dipper/setup-makefiles.sh"
+extract_files="/home/alk/yaap/device/xiaomi/dipper/extract-files.sh"
 
 website_curl()
 {
